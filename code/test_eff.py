a099579ccdb7568cd30e99621c3306e1d6d6ce22
Gabriel Altay <gabriel.altay@gmail.com>
2023-10-01 20:33:31 -0400

    update

--- code/test_eff.py
+++ code/test_eff.py
@@ -1,3 +1,13 @@
+import torch
+from transformers import AutoTokenizer
+from transformers import AutoModelForCausalLM
+from transformers import BitsAndBytesConfig
+from transformers import GenerationConfig
+from transformers import TextStreamer
+
+from hf_utils import BitsAndBytesConfigHelper
+from hf_utils import GenerationConfigHelper
+from llama_utils import Llama2ChatPrompt
 
 
 def load_default(model_id):
@@ -47,43 +57,30 @@ model.eval()
 l2cp = Llama2ChatPrompt()
 
 prompt = l2cp.get_prompt(
-    "Begin telling a short scifi story",
-    system_message="You are a story telling assistant. When a user asks you to begin a story, you start telling the story without replying about anything else.")
-#print(prompt)
+    "Tell me a short scifi story",
+    response_prefix="Title:",
+)
 
+DEFAULT_GENERATION_CONFIG_HELPER = GenerationConfigHelper(
+    do_sample=True,
+    top_p=0.8,
+    temperature=1.0,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=1024,
+)
 
-
-def write_story(prompt: str, max_length=1024):
-
+def get_gen_output(prompt: str, gen_helper: GenerationConfigHelper=DEFAULT_GENERATION_CONFIG_HELPER):
     tokenized = tokenizer(prompt, return_tensors="pt", add_special_tokens=False).to("cuda")
-    gen_helper = GenerationConfigHelper(
-        do_sample=True,
-        top_p=0.8,
-        temperature=1.0,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        max_length=max_length,
-    )
     generation_config = GenerationConfig(**gen_helper.model_dump())
     with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False):
         generated_ids = model.generate(**tokenized, generation_config=generation_config)
-
     out = tokenizer.batch_decode(generated_ids, skip_special_tokens=False)
     return out
 
-
-def stream_story(prompt: str, max_length=8192):
-
+def stream_gen_output(prompt: str, gen_helper: GenerationConfigHelper=DEFAULT_GENERATION_CONFIG_HELPER):
     streamer = TextStreamer(tokenizer)
     tokenized = tokenizer(prompt, return_tensors="pt", add_special_tokens=False).to("cuda")
-    gen_helper = GenerationConfigHelper(
-        do_sample=True,
-        top_p=0.8,
-        temperature=1.0,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        max_length=max_length,
-    )
     generation_config = GenerationConfig(**gen_helper.model_dump())
     with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False):
         _ = model.generate(**tokenized, generation_config=generation_config, streamer=streamer)
